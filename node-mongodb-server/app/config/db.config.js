543cd23fb51f92b460168e8842c72eda64c4b257
Passakorn Klaikaew <passakornjiew@gmail.com>
2024-05-28 22:36:49 +0700

    [ADD] nodemon and database config

--- node-mongodb-server/app/config/db.config.js
+++ node-mongodb-server/app/config/db.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  url: "mongodb://0.0.0.0:27017/bezkoder_db"
+  url: 'mongodb+srv://admin:mQNVffVamkRiAcWl@cluster0.3jumcgz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
 };
