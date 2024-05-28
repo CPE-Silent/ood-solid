03f4f098b7798e40e155d091c00288e0d9d0d035
StanG12 <stank5843@gmail.com>
2024-05-28 23:48:26 +0700

    [ADD] index

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -4,12 +4,14 @@ export interface IUser extends Document {
   name: string;
   email: string;
   password: string;
+  roles: string;
 }
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
+  roles: { type: String, required: true },
 });
 
 const User = mongoose.model<IUser>("User", userSchema);
